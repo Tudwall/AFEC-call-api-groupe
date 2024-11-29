7518becbae6f4fca43be34f59b8a392e2b903761
Pierre <pierre.laffaille@outlook.fr>
2024-11-29 16:55:11 +0100

    autre test, sans succès

--- script.js
+++ script.js
@@ -3,40 +3,52 @@ const main = document.querySelector("main");
 const nextBtn = document.getElementById("user-next-page");
 main.appendChild(userContainer);
 
-const fetchUsers = () => {
-	fetch("https://jsonplaceholder.typicode.com/users")
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error("failed to get infos");
-			}
-			return response.json();
-		})
-		.then((data) => {
-			console.log(data);
-			/* for (let i = 0; i < 5; i++) {
-				const name = document.createElement("p");
-				name.textContent = data[i].name;
-				userContainer.appendChild(name);
-				console.log(data.name);
-			} */
-			const arr = [];
-		})
-		.catch((error) => {
-			console.error("Erreur: ", error);
-		});
+let indexUser = 1;
+let indexComment = 1;
+let indexPost = 1;
+
+const displayNextUsers = (arr, min, max) => {
+	userContainer.innerHTML = "";
+	let array = arr.slice(min, max);
+	array.forEach((element) => {
+		const name = document.createElement("p");
+		name.textContent = element.name;
+		userContainer.appendChild(name);
+	});
 };
 
-fetchUsers();
+/* const change = (index, max) => {
+	index = index < max ? index : index - 1;
+	index = index > 1 ? index : index + 1;
+};
+ */
 
-function displayNextUsers() {
-	const data = fetchUsers();
-	for (let i = 4; i < 10; i++) {
-		const name = document.createElement("p");
-		name.textContent = data[i].name;
-		userContainer.removeChild();
-		userContainer.appendChild(name);
-		console.log(data.name);
+/* const fetch = (url) => {
+	fetch(`${url}`).then((response) => {
+		if (!response.ok) {
+			throw new Error("could not fetch");
+		}
+		return response.json();
+	});
+}; */
+
+const fetchUsers = () => {
+	fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+		if (!response.ok) {
+			throw new Error("failed to get infos");
+		}
+		return response.json();
+	});
+};
+
+async function loadData() {
+	try {
+		await Promise.allSettled(fetchUsers());
+	} catch (error) {
+		console.error("Une erreur inattendue s'est produite :", error.message);
 	}
 }
 
-nextBtn.addEventListener("click", displayNextUsers);
+fetchUsers(0, 5);
+
+nextBtn.addEventListener("click", console.log(fetchUsers()));
